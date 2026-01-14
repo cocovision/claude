3825f1e73722f7085e7b7277a71cfb96163a506c
techjess <jesse@cocovision.ai>
2026-01-14 09:43:54 -0800

    tr
    t

---
+++ progress/chapter2.cpp
@@ -0,0 +1,69 @@
+// #include <iostream>
+//
+// int main() {
+//     int age  = 25;
+//     short smallnum = 100;
+//     long largeNum = 10000000L;
+//     long long veryLarge = 100000LL;
+//
+//     //Unsigned versions(only pos)
+//     unsigned int count = 100;
+//
+//     //Floating types
+//     float pi = 3.14f;
+//     double precise = 3.14159265359;
+//     long double veryPrecise = 3.14159265359L;
+//
+//     //character type
+//     char letter = 'A';
+//
+//     //boolean types
+//     bool isTrue = true;
+//     bool isFalse = false;
+//
+//     std::cout << "Age: " << age << std::endl;
+//     std::cout << "Pi: " << pi << std::endl;
+//     std::cout << "Letter: " << letter << std::endl;
+//
+//     return 0;
+//}
+//_______________________________________________________________________________________________________________________
+// #include <iostream>
+//
+// int main() {
+//     int age = 25;
+//     double height = 5.9;
+//
+//     std:: cout << "Hello!" << std::endl;
+//
+//     std::cout << "Age:" << age << ", Height: " << height << std::endl;
+//
+//     //no newline
+//     std::cout << "This is ";
+//     std::cout << "one line" <<std::endl;
+//
+//     return 0;
+// }
+//_______________________________________________________________________________________________________________________
+// #include <iostream>
+//
+// int main() {
+//     int age;
+//     std:: string name;
+//
+//     std::cout << "Enter your name: ";
+//     std::cin >> name;
+//
+//     std::cout << "Enter your age: ";
+//     std::cin >> age;
+//
+//     std::cout << "Hello, " << name << "! You are " << age << " years old." << std::endl;
+//
+//     return 0;
+// }
+
+#include <iostream>
+
+int main() {
+    
+}
---
+++ progress/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+	std::cout << "22!" << std::endl;
+	std::cout << "Hello, me!" << std::endl;
+	std::cout << "Why there you are" << std::endl;
+	return 0;
+}
+
